386b469ba4b7887c1eddda1f649340fa652f5e64
Alvani Wiwoho <alpha2_yui@yahoo.com>
2016-09-07 15:15:38 +0700

    implement get name from unity shader on gltf technique

--- GlTF_Technique.cs
+++ GlTF_Technique.cs
@@ -2,6 +2,14 @@
 using System.Collections;
 
 public class GlTF_Technique : GlTF_Writer {
+
+	static public string GetNameFromShader(Shader s) 
+	{		 
+		var ret = "technique_" + s.name + "_" + s.GetInstanceID();
+		ret = ret.Replace(" ", "_");
+		return ret;
+	}
+
 	public override void Write()
 	{
 		Indent();		jsonWriter.Write ("\"" + name + "\": {\n");
