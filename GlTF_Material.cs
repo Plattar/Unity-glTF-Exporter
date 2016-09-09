7f69bf4e2283ef6a365c454d5917d1e002bf886e
Alvani Wiwoho <alpha2_yui@yahoo.com>
2016-09-09 16:04:54 +0700

    Implement writing values on material and add ColorValue class

--- GlTF_Material.cs
+++ GlTF_Material.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GlTF_Material : GlTF_Writer {
+
+	public class Value : GlTF_Writer {		
+	}
+
+	public class ColorValue : Value {
+		public Color color;
+
+		public override void Write()
+		{
+			jsonWriter.Write ("\"" + name + "\": [");
+			jsonWriter.Write (color.r.ToString() + ", " + color.g.ToString() + ", " +color.b.ToString() + ", " + color.a.ToString());
+			jsonWriter.Write ("]");
+		}
+	}
 
 	public string instanceTechniqueName = "technique1";
 	public GlTF_ColorOrTexture ambient;// = new GlTF_ColorRGBA ("ambient");
 	public GlTF_ColorOrTexture diffuse;
 	public float shininess;
 	public GlTF_ColorOrTexture specular;// = new GlTF_ColorRGBA ("specular");
+	public List<Value> values = new List<Value>();
 
 	public static string GetNameFromObject(Object o) 
 	{		 		
@@ -22,24 +38,33 @@ public class GlTF_Material : GlTF_Writer {
 		Indent();		jsonWriter.Write ("\"technique\": \"" + instanceTechniqueName + "\",\n");
 		Indent();		jsonWriter.Write ("\"values\": {\n");
 		IndentIn();
-		if (ambient != null)
+		foreach (var v in values) 
 		{
 			CommaNL();
-			ambient.Write ();
+			Indent();	v.Write();
 		}
-		if (diffuse != null)
-		{
-			CommaNL();
-			diffuse.Write ();
-		}
-		CommaNL();
-		Indent();		jsonWriter.Write ("\"shininess\": " + shininess);
-		if (specular != null)
-		{
-			CommaNL();
-			specular.Write ();
-		}
-		jsonWriter.WriteLine();
+
+
+//		if (ambient != null)
+//		{
+//			CommaNL();
+//			ambient.Write ();
+//		}
+//		if (diffuse != null)
+//		{
+//			CommaNL();
+//			diffuse.Write ();
+//		}
+//		CommaNL();
+//		Indent();		jsonWriter.Write ("\"shininess\": " + shininess);
+//		if (specular != null)
+//		{
+//			CommaNL();
+//			specular.Write ();
+//		}
+//		jsonWriter.WriteLine();
+
+		Indent();		jsonWriter.Write ("\n");
 		IndentOut();
 		Indent();		jsonWriter.Write ("}");
 		CommaNL();
