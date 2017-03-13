faecfdd8d2492cefce1b086880552723b29385b8
Aurélien Chatelain <aurelien@sketchfab.com>
2017-03-13 11:43:02 -0400

    [#D3D-2966] Replaces target.id by target.node
    See https://github.com/KhronosGroup/glTF/issues/834

--- GlTF_Target.cs
+++ GlTF_Target.cs
@@ -8,7 +8,7 @@ public class GlTF_Target : GlTF_Writer {
 	{
 		Indent();		jsonWriter.Write ("\"" + "target" + "\": {\n");
 		IndentIn();
-		Indent();		jsonWriter.Write ("\"id\": " + GlTF_Writer.nodeNames.IndexOf(id) + ",\n");
+		Indent();		jsonWriter.Write ("\"node\": " + GlTF_Writer.nodeNames.IndexOf(id) + ",\n");
 		Indent();		jsonWriter.Write ("\"path\": \"" + path + "\"\n");
 		IndentOut();
 		Indent();		jsonWriter.Write ("}");
